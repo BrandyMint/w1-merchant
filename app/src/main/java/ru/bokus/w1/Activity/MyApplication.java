2a3ffc8052e8c9b092b3bfe088c7449186e59050
Alexey Illarionov <littlesavage@rambler.ru>
2015-01-20 17:37:34 +0300

    Нафиг акру

--- app/src/main/java/ru/bokus/w1/Activity/MyApplication.java
+++ app/src/main/java/ru/bokus/w1/Activity/MyApplication.java
@@ -2,19 +2,10 @@ package ru.bokus.w1.Activity;
 
 import android.app.Application;
 
-import org.acra.ACRA;
-import org.acra.ACRAConfiguration;
-import org.acra.annotation.ReportsCrashes;
-
-@ReportsCrashes(formKey = "",formUri = "http://37.252.0.113/kassa/")
 public class MyApplication extends Application {
   @Override
   public void onCreate() {
     // The following line triggers the initialization of ACRA
     super.onCreate();
-    ACRAConfiguration acraConfiguration = ACRA.getNewDefaultConfig(this);    
-    acraConfiguration.setFormUri("http://37.252.0.113/kassa/index.php");
-    ACRA.setConfig(acraConfiguration);
-    ACRA.init(this);
   }
 }	
