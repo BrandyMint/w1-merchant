81b4fcee593645b86dbe8441a37204a1ae2937d4
Alexey Illarionov <littlesavage@rambler.ru>
2015-02-02 21:14:45 +0300

    StrictPolicy

--- app/src/main/java/com/w1/merchant/android/Application.java
+++ app/src/main/java/com/w1/merchant/android/Application.java
@@ -1,10 +1,25 @@
 package com.w1.merchant.android;
 
+import android.os.StrictMode;
+
 import com.w1.merchant.android.utils.NetworkUtils;
 
 public class Application extends android.app.Application {
     @Override
     public void onCreate() {
+        if ("debug".equals(BuildConfig.BUILD_TYPE)) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyFlashScreen()
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                            // .penaltyDeath()
+                    .build());
+        }
+
         super.onCreate();
         NetworkUtils.getInstance().onAppInit(this);
     }
