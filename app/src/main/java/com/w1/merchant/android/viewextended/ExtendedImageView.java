59e96369225bca2d2eaa6d82780ac884f150f486
Alexey Illarionov <littlesavage@rambler.ru>
2015-03-02 12:32:07 +0300

    touch feedback при тыке на аватарке в диалоге

---
+++ app/src/main/java/com/w1/merchant/android/viewextended/ExtendedImageView.java
@@ -0,0 +1,333 @@
+package com.w1.merchant.android.viewextended;
+
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.w1.merchant.android.BuildConfig;
+import com.w1.merchant.android.R;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * ImageView, который позволяет расширять изображения при adjustViewBounds и на android 4.1
+ * + атрибут foreground
+ */
+public class ExtendedImageView extends ImageView {
+
+    private Drawable foreground;
+    private boolean mAdjustViewBounds = false;
+    private int mMaxWidth = Integer.MAX_VALUE;
+    private int mMaxHeight = Integer.MAX_VALUE;
+
+    private boolean imageViewInitialized;
+
+    public ExtendedImageView(Context context) {
+        super(context);
+    }
+
+    public ExtendedImageView(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+        initImageView(context, attrs);
+    }
+
+    public ExtendedImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initImageView(context, attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public ExtendedImageView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        initImageView(context, attrs);
+    }
+
+    private void initImageView(Context context, AttributeSet attrs) {
+        if (imageViewInitialized || attrs == null) return;
+
+        imageViewInitialized = true;
+
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ExtendedImageView);
+        Drawable foreground = a.getDrawable(R.styleable.ExtendedImageView_android_foreground);
+        if (foreground != null) {
+            setForeground(foreground);
+        }
+
+        if (a.hasValue(R.styleable.ExtendedImageView_android_maxWidth)) {
+            setMaxWidth(a.getDimensionPixelSize(R.styleable.ExtendedImageView_android_maxWidth, mMaxWidth));
+        }
+
+        if (a.hasValue(R.styleable.ExtendedImageView_android_maxHeight)) {
+            setMaxWidth(a.getDimensionPixelSize(R.styleable.ExtendedImageView_android_maxHeight, mMaxHeight));
+        }
+
+        if (a.hasValue(R.styleable.ExtendedImageView_android_adjustViewBounds)) {
+            setAdjustViewBounds(a.getBoolean(R.styleable.ExtendedImageView_android_adjustViewBounds, mAdjustViewBounds));
+        }
+
+        a.recycle();
+    }
+
+    public void setAdjustViewBounds(boolean adjustViewBounds) {
+        super.setAdjustViewBounds(adjustViewBounds);
+        mAdjustViewBounds = adjustViewBounds;
+    }
+
+    public void setMaxWidth(int maxWidth) {
+        super.setMaxWidth(maxWidth);
+        mMaxWidth = maxWidth;
+    }
+
+    public void setMaxHeight(int maxHeight) {
+        super.setMaxHeight(maxHeight);
+        mMaxHeight = maxHeight;
+    }
+
+    /**
+     * Supply a drawable resource that is to be rendered on top of all of the child
+     * views in the frame layout.
+     *
+     * @param drawableResId The drawable resource to be drawn on top of the children.
+     */
+    public void setForegroundResource(int drawableResId) {
+        setForeground(getContext().getResources().getDrawable(drawableResId));
+    }
+
+    /**
+     * Supply a Drawable that is to be rendered on top of all of the child
+     * views in the frame layout.
+     *
+     * @param drawable The Drawable to be drawn on top of the children.
+     */
+    public void setForeground(Drawable drawable) {
+        if (foreground == drawable) {
+            return;
+        }
+        if (foreground != null) {
+            foreground.setCallback(null);
+            unscheduleDrawable(foreground);
+        }
+
+        foreground = drawable;
+
+        if (drawable != null) {
+            drawable.setCallback(this);
+            if (drawable.isStateful()) {
+                drawable.setState(getDrawableState());
+            }
+        }
+        requestLayout();
+        invalidate();
+    }
+
+    @Override
+    protected boolean verifyDrawable(Drawable who) {
+        return super.verifyDrawable(who) || who == foreground;
+    }
+
+    @Override
+    public void jumpDrawablesToCurrentState() {
+        super.jumpDrawablesToCurrentState();
+        if (foreground != null) foreground.jumpToCurrentState();
+    }
+
+    @Override
+    protected void drawableStateChanged() {
+        super.drawableStateChanged();
+        if (foreground != null && foreground.isStateful()) {
+            foreground.setState(getDrawableState());
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void assertParams() {
+        if (BuildConfig.DEBUG) {
+            assertEquals("maxWidth должен быть установлен в коде", mMaxWidth, getMaxWidth());
+            assertEquals("maxHeight должен быть установлен в коде", mMaxHeight, getMaxHeight());
+            assertEquals("adjustViewBounds должен быть установлен в коде", mAdjustViewBounds, getAdjustViewBounds());
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        if (Build.VERSION.SDK_INT  <= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            onMeasureApiBelow17(widthMeasureSpec, heightMeasureSpec);
+        }
+
+        if (foreground != null) {
+            foreground.setBounds(0, 0, getMeasuredWidth(), getMeasuredHeight());
+            invalidate();
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (foreground != null) {
+            foreground.setBounds(0, 0, w, h);
+            invalidate();
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+
+        if (foreground != null) {
+            foreground.draw(canvas);
+        }
+    }
+
+    private void onMeasureApiBelow17(int widthMeasureSpec, int heightMeasureSpec) {
+        if (Build.VERSION.SDK_INT > 16) assertParams();
+
+        int w;
+        int h;
+
+        // Desired aspect ratio of the view's contents (not including padding)
+        float desiredAspect = 0.0f;
+
+        // We are allowed to change the view's width
+        boolean resizeWidth = false;
+
+        // We are allowed to change the view's height
+        boolean resizeHeight = false;
+
+        final int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+        final int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        w = h = 0;
+        Drawable d = getDrawable();
+        if (d != null) {
+            w = d.getIntrinsicWidth();
+            h = d.getIntrinsicHeight();
+            if (w <= 0) w = 1;
+            if (h <= 0) h = 1;
+
+            // We are supposed to adjust view bounds to match the aspect
+            // ratio of our drawable. See if that is possible.
+            if (mAdjustViewBounds) {
+                resizeWidth = widthSpecMode != MeasureSpec.EXACTLY;
+                resizeHeight = heightSpecMode != MeasureSpec.EXACTLY;
+
+                desiredAspect = (float) w / (float) h;
+            }
+        }
+
+        int pleft = getPaddingLeft();
+        int pright = getPaddingRight();
+        int ptop = getPaddingTop();
+        int pbottom = getPaddingBottom();
+
+        int widthSize;
+        int heightSize;
+
+        if (resizeWidth || resizeHeight) {
+            /* If we get here, it means we want to resize to match the
+                drawables aspect ratio, and we have the freedom to change at
+                least one dimension.
+            */
+
+            // Get the max possible width given our constraints
+            widthSize = resolveAdjustedSize(w + pleft + pright, mMaxWidth, widthMeasureSpec);
+
+            // Get the max possible height given our constraints
+            heightSize = resolveAdjustedSize(h + ptop + pbottom, mMaxHeight, heightMeasureSpec);
+
+            if (desiredAspect != 0.0f) {
+                // See what our actual aspect ratio is
+                float actualAspect = (float)(widthSize - pleft - pright) /
+                        (heightSize - ptop - pbottom);
+
+                if (Math.abs(actualAspect - desiredAspect) > 0.0000001) {
+
+                    boolean done = false;
+
+                    // Try adjusting width to be proportional to height
+                    if (resizeWidth) {
+                        int newWidth = (int)(desiredAspect * (heightSize - ptop - pbottom)) +
+                                pleft + pright;
+
+                        // Allow the width to outgrow its original estimate if height is fixed.
+                        if (!resizeHeight) {
+                            widthSize = resolveAdjustedSize(newWidth, mMaxWidth, widthMeasureSpec);
+                        }
+
+                        if (newWidth <= widthSize) {
+                            widthSize = newWidth;
+                            done = true;
+                        }
+                    }
+
+                    // Try adjusting height to be proportional to width
+                    if (!done && resizeHeight) {
+                        int newHeight = (int)((widthSize - pleft - pright) / desiredAspect) +
+                                ptop + pbottom;
+
+                        // Allow the height to outgrow its original estimate if width is fixed.
+                        if (!resizeWidth) {
+                            heightSize = resolveAdjustedSize(newHeight, mMaxHeight,
+                                    heightMeasureSpec);
+                        }
+
+                        if (newHeight <= heightSize) {
+                            heightSize = newHeight;
+                        }
+                    }
+                }
+            }
+        } else {
+            /* We are either don't want to preserve the drawables aspect ratio,
+               or we are not allowed to change view dimensions. Just measure in
+               the normal way.
+            */
+            w += pleft + pright;
+            h += ptop + pbottom;
+
+            w = Math.max(w, getSuggestedMinimumWidth());
+            h = Math.max(h, getSuggestedMinimumHeight());
+
+            widthSize = resolveSizeAndState(w, widthMeasureSpec, 0);
+            heightSize = resolveSizeAndState(h, heightMeasureSpec, 0);
+        }
+
+        setMeasuredDimension(widthSize, heightSize);
+    }
+
+    private int resolveAdjustedSize(int desiredSize, int maxSize,
+                                    int measureSpec) {
+        int result = desiredSize;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize =  MeasureSpec.getSize(measureSpec);
+        switch (specMode) {
+            case MeasureSpec.UNSPECIFIED:
+                /* Parent says we can be as big as we want. Just don't be larger
+                   than max size imposed on ourselves.
+                */
+                result = Math.min(desiredSize, maxSize);
+                break;
+            case MeasureSpec.AT_MOST:
+                // Parent says we can be as big as we want, up to specSize.
+                // Don't be larger than specSize, and don't be larger than
+                // the max size imposed on ourselves.
+                result = Math.min(Math.min(desiredSize, specSize), maxSize);
+                break;
+            case MeasureSpec.EXACTLY:
+                // No choice. Do what we are told.
+                result = specSize;
+                break;
+        }
+        return result;
+    }
+
+
+}
