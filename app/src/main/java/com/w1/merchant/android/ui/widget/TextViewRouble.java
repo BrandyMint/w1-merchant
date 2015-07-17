3199ba867986b7fdd1c599a727c283ae2bc7976d
Alexey Illarionov <littlesavage@rambler.ru>
2015-07-17 12:27:54 +0300

    Исправлено обрезание символов на android < 4.1 в диалоге окончания выставления счета Fixes #8486

--- app/src/main/java/com/w1/merchant/android/ui/widget/TextViewRouble.java
+++ app/src/main/java/com/w1/merchant/android/ui/widget/TextViewRouble.java
@@ -1,13 +1,17 @@
 package com.w1.merchant.android.ui.widget;
 
 import android.content.Context;
+import android.support.v4.widget.TextViewCompat;
+import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
 import com.w1.merchant.android.utils.FontManager;
 
-//Расширение TextView для отображения шрифта со знаком рубля 
-public class TextViewRouble extends TextView {
+//Расширение TextView для отображения шрифта со знаком рубля
+// TODO: 17.07.15 избавиться
+@Deprecated
+public class TextViewRouble extends AppCompatTextView {
 
     private boolean initialized;
 
